326bca1f6200dda0dd977ead685f6984aefe14b7
Ajay Kumar <ajaykumar@Ajays-MacBook-Pro.local>
2024-08-15 07:22:46 +0530

    Dare2Create

---
+++ bin/dsa_with_dart.dart
@@ -0,0 +1,5 @@
+import 'package:dsa_with_dart/dsa_with_dart.dart' as dsa_with_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dsa_with_dart.calculate()}!');
+}
---
+++ test/dsa_with_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:dsa_with_dart/dsa_with_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
